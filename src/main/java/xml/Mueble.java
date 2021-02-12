1ba539a86098c60cb1388c8377103ed532463d04
JCarlosVico <jfervic933@maralboran.es>
2021-02-12 14:24:18 +0100

    Ficheros XML con JAXB

---
+++ src/main/java/xml/Mueble.java
@@ -0,0 +1,66 @@
+package xml;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author J.Carlos
+ */
+
+// Anotación @XmlRootElement, nombre de la etiqueta XML raíz.
+@XmlRootElement(name = "mueble")
+public class Mueble {
+
+    private String id;
+    private Double ancho;
+    private Double alto;
+    private Double profundo;
+
+    public Mueble(int id) {
+        this.id = "ES-M" + id;
+        ancho = alto = profundo = Double.valueOf(id);
+    }
+
+    public Mueble() {
+        id = "ES-M";
+        ancho = alto = profundo = 1.0;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Double getAncho() {
+        return ancho;
+    }
+
+    public void setAncho(double ancho) {
+        this.ancho = ancho;
+    }
+
+    public Double getAlto() {
+        return alto;
+    }
+
+    public void setAlto(double alto) {
+        this.alto = alto;
+    }
+
+    public Double getProfundo() {
+        return profundo;
+    }
+
+    public void setProfundo(double profundo) {
+        this.profundo = profundo;
+    }
+
+    @Override
+    public String toString() {
+        return "Mueble{" + "id=" + id + ", ancho=" + ancho + ", alto=" + alto + ", profundo=" + profundo + '}';
+
+    }
+}
